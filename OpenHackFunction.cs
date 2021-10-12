859af37de5817c9d8f90f236180210de1e2a1c15
Cedric FRONTEAU <Cedric.FRONTEAU@edenred.com>
2021-10-12 16:38:02 +0200

    feat: Create CreateRating function.

--- OpenHackFunction.cs
+++ OpenHackFunction.cs
@@ -6,33 +6,56 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-//using Newtonsoft.Json;
 using System.Text.Json;
 using System.Dynamic;
+using RestSharp;
 
 namespace Company.Function
 {
-    public static class OpenHackFunction
+    public static class CreateRating
     {
-        [FunctionName("OpenHackFunction")]
+        [FunctionName("CreateRating")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "%CosmosDb.Database%",
+                collectionName: "%CosmosDb.Collection%",
+                ConnectionStringSetting = "CosmosDb.ConnectionString")] IAsyncCollector<dynamic> documentsOut,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            dynamic data = await JsonSerializer.DeserializeAsync<ExpandoObject>(req.Body);
+            string productId = data?.productId.GetString();
+            string userId = data?.userId.GetString();
+            int rating = data?.rating.GetInt32();
 
-            string productId = req.Query["productId"];
+            // Call GetProduct API
+            string getProductUrl = "https://serverlessohapi.azurewebsites.net";
 
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //dynamic data = JsonSerializer.DeserializeAsync<ExpandoObject>(req.Body);
-            //string productId = data?.productId;
+            var client = new RestClient(getProductUrl);
+            var request = new RestRequest("/api/GetProduct", Method.GET);
+            request.AddQueryParameter("productId", productId);
+            IRestResponse response = await client.ExecuteAsync(request);
+            if (rating <= 5 && rating >=0 && response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                request = new RestRequest("/api/GetUser", Method.GET);
+                request.AddQueryParameter("userId", userId);
+                response = await client.ExecuteAsync(request);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    await documentsOut.AddAsync(new {
+                        id = Guid.NewGuid(),
+                        timestamp = DateTime.UtcNow.ToString("o"),
+                        userId = userId,
+                        productId = productId,
+                        rating = rating,
+                        locationName = data?.locationName.GetString(),
+                        userNotes = data?.userNotes.GetString()
+                    });
+                    return new OkObjectResult("success");
+                }
+            }
 
-            string responseMessage = string.IsNullOrEmpty(productId)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"The product name for your product id {productId} is Starfruit Explosion";
-
-            return new OkObjectResult(responseMessage);
+            return new BadRequestResult();
         }
     }
 }
