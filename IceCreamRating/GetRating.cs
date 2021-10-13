e704019b7cf19ce7143be4b13dc9911a1f4436d6
BASCOUL Nicolas (Consulting for Edenred) <Nicolas.BASCOUL@consulting-for.edenred.com>
2021-10-13 14:50:43 +0200

    Fix binding

--- IceCreamRating/GetRating.cs
+++ IceCreamRating/GetRating.cs
@@ -1,17 +1,16 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+
+
+namespace Company.Function
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc; 
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Text.Json;
-using System.Dynamic;
-using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Company.Function
-{
     public static class GetRating
     {
         [FunctionName("GetRating")]
@@ -24,9 +23,9 @@ namespace Company.Function
                 collectionName: "%CosmosDb.Collection%",
                 ConnectionStringSetting = "CosmosDb.ConnectionString",
                 Id = "{ratingId}",
-                PartitionKey = "Id"
-                )] IAsyncCollector<dynamic> documentOut,
-            ILogger log)
+                PartitionKey = "{ratingId}"
+                )] RatingItem documentOut,
+            ILogger log) 
         {
             
             return documentOut != null ? (IActionResult)new OkObjectResult(documentOut) : new NotFoundObjectResult($@"No rating found");
