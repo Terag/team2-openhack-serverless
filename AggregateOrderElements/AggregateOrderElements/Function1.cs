f0e60922ef3b7568e22e7861a888077023f4366f
Cedric FRONTEAU <Cedric.FRONTEAU@edenred.com>
2021-10-14 13:28:41 +0200

    NewFile function.

--- AggregateOrderElements/AggregateOrderElements/Function1.cs
+++ AggregateOrderElements/AggregateOrderElements/Function1.cs
@@ -1,39 +1,24 @@
 using System;
 using System.IO;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace AggregateOrderElements
 {
-    public static class Function1
+    public static class NewFile
     {
-        [FunctionName("Function1")]
-        public static void Run([BlobTrigger("orders/{name}", Connection = "DefaultEndpointsProtocol=https;AccountName=bfyocstorageaccount;AccountKey=3TaClxdiwSGsxQs46GFSNMS92jEtvBAxzfnWuhWWfxjFva5TYdLlhGsXLCNkHOnyreyoEOMFWHfdz7FqW52cDA==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
+        [FunctionName("NewFile")]
+        public static void Run([BlobTrigger("orders/{orderId}-{file}", 
+            Connection = "DefaultEndpointsProtocol=https;AccountName=bfyocstorageaccount;AccountKey=3TaClxdiwSGsxQs46GFSNMS92jEtvBAxzfnWuhWWfxjFva5TYdLlhGsXLCNkHOnyreyoEOMFWHfdz7FqW52cDA==;EndpointSuffix=core.windows.net")]Stream myBlob, 
+            string orderId, 
+            string file,
+            IDurableOrchestrationClient starter,
+            ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-        }
-
-        public static string ExtractId(string name)
-        {
-            return "";
-        }
-
-        public static void Store()
-        {
-            //TO DO
-        }
-
-        public static void CombineOrder()
-        {
-            //TO DO
-        }
-
-        // Retrieve the state, save the new state, if the state is complete, it triggers CombineOrder and Store the final result.
-        public static void Orchestrator()
-        {
-            //TO DO
+            starter.StartNewAsync("Orchestrator", orderId, file);
         }
     }
 }
